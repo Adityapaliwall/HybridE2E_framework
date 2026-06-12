5a73bcff3a9fe93af06b114720a3d917fc62f52b
Adityapaliwall <your-email>
2026-06-12 11:10:37 +0530

    adding allure

--- tests/api/test_create_note_api.py
+++ tests/api/test_create_note_api.py
@@ -1,12 +1,19 @@
 from api.notes.notes_api import NotesAPI
 from utils.read_data import read_json
+import allure
 
 
+@allure.feature("Notes API")
+@allure.story("Create Note - Positive")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_create_note(headers):
+
     payload = read_json("test_data/create_note.json")
 
     response = NotesAPI().create_note(headers, payload)
+
     assert response.status_code == 200
+
     data = response.json()["data"]
 
     assert data["title"] == payload["title"]
@@ -15,7 +22,12 @@ def test_create_note(headers):
 
     print("\n----------------Note created successfully-------------")
 
+
+@allure.feature("Notes API")
+@allure.story("Create Note - Negative")
+@allure.severity(allure.severity_level.NORMAL)
 def test_create_note_invalid_data(headers):
+
     payload = read_json("test_data/create_note_invalid.json")
 
     response = NotesAPI().create_note(headers, payload)
@@ -24,4 +36,4 @@ def test_create_note_invalid_data(headers):
     print(response.status_code)
     print(response.text)
 
-    assert response.status_code in [400,422]
+    assert response.status_code in [400, 422]
